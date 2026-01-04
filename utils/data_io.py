7160c507f47b8f71c9c620b228c0647d9e33c549
Bjarne Lund Pedersen <96906340+Bjarnelp@users.noreply.github.com>
2026-01-04 12:24:17 +0100

    Updated data io class to read both input and output paths (#3)
    * Updated data io class to read both input and output paths
    
    * Updated according to code review

--- utils/data_io.py
+++ utils/data_io.py
@@ -4,31 +4,38 @@ import pathlib
 
 
 class DataIO:
-    def __init__(self, path: str) -> None:
-        self.path = path
+    def __init__(self, input_path: str, output_path: str):
+        self.input_path = input_path
+        self.output_path = output_path
 
-    def load_data(self) -> pl.DataFrame:
-        if not self.path:
+    def load_data(self, file_name: str) -> pl.DataFrame:
+        if not self.input_path:
             raise ValueError("Data path must be provided.")
-        if not isinstance(self.path, str):
+        if not isinstance(self.input_path, str):
             raise TypeError("Data path must be a string.")
+        if not os.path.exists(self.input_path):
+            raise FileNotFoundError(f"Data path '{self.input_path}' does not exist.")
 
-        self.path = pathlib.Path(self.path).as_posix()
+        input_path = pathlib.Path(self.input_path).as_posix()
+
+        full_input_path = os.path.join(input_path, file_name)
+        if not os.path.isfile(full_input_path):
+            raise FileNotFoundError(f"File '{full_input_path}' does not exist.")
 
         # Get data type from file extension
-        if self.path.endswith(".parquet"):
-            return pl.read_parquet(self.path)
-        elif self.path.endswith(".csv"):
-            return pl.read_csv(self.path)
+        if full_input_path.endswith(".parquet"):
+            return pl.read_parquet(full_input_path)
+        elif full_input_path.endswith(".csv"):
+            return pl.read_csv(full_input_path)
         else:
             raise ValueError(
                 "Unsupported file format. Please use .parquet or .csv files."
             )
 
-    def save_data(self, df: pl.DataFrame, output_path: str, file_name: str) -> None:
-        if not output_path:
+    def save_data(self, df: pl.DataFrame, file_name: str) -> None:
+        if not self.output_path:
             raise ValueError("Output path must be provided.")
-        if not isinstance(output_path, str):
+        if not isinstance(self.output_path, str):
             raise TypeError("Output path must be a string.")
 
         if not file_name:
@@ -38,11 +45,11 @@ class DataIO:
         if not file_name.endswith(".parquet"):
             raise ValueError("File name must end with .parquet extension.")
 
-        output_path = pathlib.Path(output_path).as_posix()
+        output_path = pathlib.Path(self.output_path).as_posix()
 
         pathlib.Path(output_path).mkdir(parents=True, exist_ok=True)
 
-        full_output_path = f"{output_path}/{file_name}"
+        full_output_path = os.path.join(output_path, file_name)
         if os.path.isfile(full_output_path):
             raise FileExistsError(f"File already exists at '{full_output_path}'.")
 
@@ -55,11 +62,9 @@ class DataIO:
 
 
 if __name__ == "__main__":
-    data_io = DataIO("./data/yellow_tripdata_2025-06.parquet")
-    tripdata = data_io.load_data()
+    data_io = DataIO(input_path="./data/", output_path="./output/raw/")
+    tripdata = data_io.load_data(file_name="yellow_tripdata_2025-06.parquet")
 
     tripdata.head(5).show()
 
-    output_dir = "./output/source/"
-    file_name = "tripdata_sample.parquet"
-    data_io.save_data(tripdata.head(1000), output_dir, file_name)
+    data_io.save_data(tripdata.head(1000), file_name="tripdata_sample.parquet")
